8576dc7143bcf0f6819ac5324446847850f9621c
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2024-01-19 16:00:12 -0600

    Initial Commit

---
+++ video-app/video_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_appApp.swift
+//  video-app
+//
+//  Created by Jorge on 19/01/24.
+//
+
+import SwiftUI
+
+@main
+struct video_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
